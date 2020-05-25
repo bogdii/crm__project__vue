1eb310874e49f2445b1e7e802aa918cab1229b9b
bogdii7 <protsenkobogdan@gmail.com>
2020-05-25 02:09:34 +0200

    pagination & tooltip

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
+import Paginate from 'vuejs-paginate'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import dateFilter from './filters/date.filter'
 import currencyFilter from './filters/currency.filter'
+import tooltipDirective from './directives/tooltip.directive'
 import messagePlugin from './utils/message.plugin'
 import Loader from './components/app/Loader'
 import 'materialize-css/dist/js/materialize.min'
@@ -20,7 +22,9 @@ Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
+Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
 
 
 
